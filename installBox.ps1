4572fc5c036232946257c751a0b1b3f3a507908b
tpodolak <misiek767@gmail.com>
2016-12-06 14:52:15 -0800

    Allow empty checksums

--- installBox.ps1
+++ installBox.ps1
@@ -70,7 +70,7 @@ function Install-Local-Packages ($packages, $installedPackages){
 
 function Install-Choco-Packages ($packages){
     foreach ($package in $packages) {
-        cinst $package
+        cinst $package --allow-empty-checksums
     }
 }
 
